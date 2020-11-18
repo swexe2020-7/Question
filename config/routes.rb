98d1fa30f3c718ec1e87f9b60286ce77cbd9165f
funabikitoshinori <1811140053m@ed.fuk.kindai.ac.jp>
2020-11-18 04:44:04 +0000

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'questions/index'
   get 'questions/new'
+  get 'questions/create'
+  get 'questions/destroy'
   get 'questions/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -11,4 +11,14 @@ class QuestionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get create" do
+    get questions_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get questions_destroy_url
+    assert_response :success
+  end
+
 end
