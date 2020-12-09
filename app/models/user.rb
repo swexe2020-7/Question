91ff2518b711499ec44694930139b847f0eb89d3
EC2 Default User <ec2-user@ip-172-31-24-92.ec2.internal>
2020-12-09 06:20:17 +0000

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
     has_many :tweets
     has_many :likes
     has_many :like_tweets, through: :likes, source: :tweet    
     
     validates :uid, presence: true, uniqueness: :true
     validates :pass, presence: true    
-=======
     has_many :questions
     
     validates :uid, presence: true, uniqueness: :true
     validates :pass, presence: true
->>>>>>> 55cad86ef181f7731cfda939ffb93d5c4808eae8
     
     def self.authenticate(uid, pass)
         user = find_by(uid: uid)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :homes
+  resources :users
     root 'homes#index'
     get 'homes/index'
     get 'homes/new',to: 'homes#new'
@@ -9,20 +9,6 @@ Rails.application.routes.draw do
     get 'homes/:id' , to: 'homes#show'
     get 'homes/:id/edit', to: 'homes#edit'
     patch 'homes/:id', to: 'homes#update'
-    
-  
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-=======
-  root 'users#index'
-  get 'users/index'
-  get 'users/new'
-  get 'questions/index'
-  get 'questions/new'
-  get 'questions/create'
-  get 'questions/destroy'
-  get 'questions/index'
-  resources :users
-end
->>>>>>> 55cad86ef181f7731cfda939ffb93d5c4808eae8
+    get 'logins/login_form'
+    post 'logins/login'
+  end
